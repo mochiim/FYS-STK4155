f40c4d40438f43f1d293554ebfd2463c386dd784
mochiim <43903898+mochiim@users.noreply.github.com>
2023-09-06 14:31:54 +0200

    Push

--- trial.py
+++ trial.py
@@ -1,68 +1,78 @@
+import os
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import Ridge, LinearRegression, Lasso, Ridge
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
 
-np.random.seed(4155)
+def R2(y_data, y_model):
+    return 1 - np.sum((y_data - y_model) ** 2) / np.sum((y_data - np.mean(y_data)) ** 2)
+def MSE(y_data,y_model):
+    n = np.size(y_model)
+    return np.sum((y_data-y_model)**2)/n
 
-### a)
 
-def FrankeFunction(x,y):
-    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-    return term1 + term2 + term3 + term4
+# A seed just to ensure that the random numbers are the same for every run.
+# Useful for eventual debugging.
+np.random.seed(3155)
 
-# Generate the data
-nrow = 100
-ncol = 200
-ax_row = np.random.uniform(0, 1, size=nrow)
-ax_col = np.random.uniform(0, 1, size=ncol)
+x = np.random.rand(100)
+y = 2.0+5*x*x+0.1*np.random.randn(100)
 
-ind_sort_row = np.argsort(ax_row)
-ind_sort_col = np.argsort(ax_col)
+# number of features p (here degree of polynomial
+p = 3
+#  The design matrix now as function of a given polynomial
+X = np.zeros((len(x),p))
+X[:,0] = 1.0
+X[:,1] = x
+X[:,2] = x*x
+# We split the data in test and training data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-ax_row_sorted = ax_row[ind_sort_row]
-ax_col_sorted = ax_col[ind_sort_col]
+# matrix inversion to find beta
+OLSbeta = np.linalg.inv(X_train.T @ X_train) @ X_train.T @ y_train
+print(OLSbeta)
+# and then make the prediction
+ytildeOLS = X_train @ OLSbeta
+print("Training MSE for OLS")
+print(MSE(y_train,ytildeOLS))
+ypredictOLS = X_test @ OLSbeta
+print("Test MSE OLS")
+print(MSE(y_test,ypredictOLS))
 
-colmat, rowmat = np.meshgrid(ax_col_sorted, ax_row_sorted)
+# Repeat now for Lasso and Ridge regression and various values of the regularization parameter
+I = np.eye(p,p)
+# Decide which values of lambda to use
+nlambdas = 100
+MSEPredict = np.zeros(nlambdas)
+MSETrain = np.zeros(nlambdas)
+MSELassoPredict = np.zeros(nlambdas)
+MSELassoTrain = np.zeros(nlambdas)
+lambdas = np.logspace(-4, 4, nlambdas)
+for i in range(nlambdas):
+    lmb = lambdas[i]
+    Ridgebeta = np.linalg.inv(X_train.T @ X_train+lmb*I) @ X_train.T @ y_train
+    # include lasso using Scikit-Learn
+    RegLasso = linear_model.Lasso(lmb,fit_intercept=False)
+    RegLasso.fit(X_train,y_train)
+    # and then make the prediction
+    ytildeRidge = X_train @ Ridgebeta
+    ypredictRidge = X_test @ Ridgebeta
+    ytildeLasso = RegLasso.predict(X_train)
+    ypredictLasso = RegLasso.predict(X_test)
+    MSEPredict[i] = MSE(y_test,ypredictRidge)
+    MSETrain[i] = MSE(y_train,ytildeRidge)
+    MSELassoPredict[i] = MSE(y_test,ypredictLasso)
+    MSELassoTrain[i] = MSE(y_train,ytildeLasso)
 
-noise_str = .0
-noise = np.random.randn(nrow, ncol)
+# Now plot the results
+plt.figure()
+plt.plot(np.log10(lambdas), MSETrain, label = 'MSE Ridge train')
+plt.plot(np.log10(lambdas), MSEPredict, 'r--', label = 'MSE Ridge Test')
+plt.plot(np.log10(lambdas), MSELassoTrain, label = 'MSE Lasso train')
+plt.plot(np.log10(lambdas), MSELassoPredict, 'r--', label = 'MSE Lasso Test')
 
-z = FrankeFunction(rowmat, colmat) + noise_str * noise
-
-row_arr = rowmat.ravel()
-col_arr = colmat.ravel()
-z_arr = z.ravel()
-
-# Generate the design matrix
-p = 10
-poly = PolynomialFeatures(degree = p)
-X = poly.fit_transform(np.c_[row_arr, col_arr])
-
-## Perform OLS
-linreg = LinearRegression()
-linreg.fit(X, z_arr)
-
-zpred = linreg.predict(X)
-zplot = zpred.reshape(nrow, ncol)
-
-# Plot the reuslting fit beside the original surface
-fig = plt.figure()
-
-ax = fig.add_subplot(1, 2, 1, projection='3d')
-surf = ax.plot_surface(colmat, rowmat, z, cmap=cm.viridis, linewidth=0, antialiased=False)
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.title('Franke')
-
-ax = fig.add_subplot(1, 2, 2, projection='3d')
-surf = ax.plot_surface(colmat, rowmat, zplot, cmap=cm.viridis, linewidth=0, antialiased=False)
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.title('Fitted Franke')
-
+plt.xlabel('log10(lambda)')
+plt.ylabel('MSE')
+plt.legend()
 plt.show()
