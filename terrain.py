105453fb7cb9ae7dc88afcae7e31ecf920aed9b1
mochiim <43903898+mochiim@users.noreply.github.com>
2023-10-14 17:47:37 +0200

    Terrain

--- terrain.py
+++ terrain.py
@@ -1,22 +1,25 @@
 import numpy as np
-import imageio 
-import matplotlib.pyplot as plt
+from imageio import imread
 from mpl_toolkits.mplot3d import Axes3D
-
-import tifffile as tiff
-import scipy as sc
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
 # Load the terrain
-terrain1 = imageio.imread('SRTM_data_Norway_1.tif')
+terrain = imread('SRTM_data_Norway_1.tif')
 
+N = 1000
+m = 5 # polynomial orderx
+terrain = terrain[:N,:N]
+# Creates mesh of image pixels
+x = np.linspace(0, 1, np.shape(terrain)[0])
+y = np.linspace(0, 1, np.shape(terrain)[1])
+x_mesh, y_mesh = np.meshgrid(x,y)
 
-# Show the terrain
-fig, ax = plt.subplots(figsize = (5, 6))
-ax.set_title('Terrain over Norway')
-image = plt.imshow(terrain1, cmap = 'seismic')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-fig.colorbar(image)
-fig.tight_layout()
-#plt.savefig('terrain1.png')
+z = terrain
+
+fig = plt.figure(figsize = (8, 6)) 
+ax = fig.gca(projection='3d') 
+surf = ax.plot_surface(x_mesh, y_mesh, z, cmap = cm.coolwarm, linewidth=0, antialiased=False)
+fig.colorbar(surf, shrink=0.5, aspect=5)
+plt.savefig('terrain_3D.png')
 plt.show()
